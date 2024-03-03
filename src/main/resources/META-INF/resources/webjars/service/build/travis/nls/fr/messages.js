12664598bf67a16aaa872bdf96a06a39bc8067a3
Fabrice Daugan <fabrice.daugan@gmail.com>
2024-03-03 12:41:14 +0100

    Lint i18n

--- src/main/resources/META-INF/resources/webjars/service/build/travis/nls/fr/messages.js
+++ src/main/resources/META-INF/resources/webjars/service/build/travis/nls/fr/messages.js
@@ -4,18 +4,18 @@ define({
 	'service:build:travis:url-api-description': 'API endpoint à utiliser, voir <a href="https://docs.travis-ci.com/api">docs.travis-ci.com</a>',
 	'service:build:travis:url-site': 'URL',
 	'service:build:travis:url-site-description': 'Web URL. Pour les projets open source utiliser https://travis-ci.org/',
-	'service:build:travis:api-token-description': 'Executer : gem install travis && travis login && travis token',
+	'service:build:travis:api-token-description': 'Exécuter : gem install travis && travis login && travis token',
 	'service:build:travis:build': 'Construire',
 	'service:build:travis:status-blue': 'Succès',
 	'service:build:travis:status-yellow': 'Instable',
 	'service:build:travis:status-disabled': 'Inconnu',
-	'service:build:travis:status-red': 'Echec',
+	'service:build:travis:status-red': 'Échec',
 	'service:build:travis:building': 'En construction',
 	'travis-build-job-success': 'Lancement du job {{this}} effectué',
 	'error': {
 		'travis-job': 'Tâche non trouvée',
 		'travis-connection': 'Serveur inatteignable',
-		'travis-login': 'Echec de l\'authentification',
+		'travis-login': 'Échec de l\'authentification',
 		'travis-rights': 'Droits insuffisants pour accéder aux tâches'
 	},
 	'validation-job-name' : 'Doit commencer par {{this}}-, ne contenir que des caractères minuscules, sans caractères spéciaux'
